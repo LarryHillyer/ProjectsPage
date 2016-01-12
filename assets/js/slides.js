aca3d34d03ab9776b69478bb4f44039cf3c9bf74
LarryHillyer <LarryHillyer@hotmail.com>
2016-01-12 12:57:19 -0700

    Copied Initial page from landscape page

---
+++ assets/js/slides.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+
+    $(".rslides").responsiveSlides({maxwidth:650,
+                                    auto: false,
+                                    pager: true});   
+
+ });
